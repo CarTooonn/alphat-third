0352305044144e08e6fef26e9a198a23563e3008
thirdza056 <33078689+thirdza056@users.noreply.github.com>
2017-12-25 23:41:51 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -98,7 +98,7 @@ class LineAPI {
   _qrCodeLogin() {
     this.setTHttpClient();
     return new Promise((resolve, reject) => {
-    this._client.getAuthQrcode(true, 'LineAlphatFork-PC',(err, result) => {
+    this._client.getAuthQrcode(true, 'THIRD-PC',(err, result) => {
       const qrcodeUrl = `line://au/q/${result.verifier}`;
       qrcode.generate(qrcodeUrl,{small: true});
       console.info(`\n\nlink qr code is: ${qrcodeUrl}`)
@@ -145,7 +145,7 @@ class LineAPI {
 				 reqx.password = rsaCrypto.credentials;
 				 reqx.keepLoggedIn = true;
 				 reqx.accessLocation = this.config.ip;
-				 reqx.systemName = 'LineAlphatFork-PC';
+				 reqx.systemName = 'THIRD-PC';
 				 reqx.e2eeVersion = 0;
 				 try{
 					 this._client.loginZ(reqx,
